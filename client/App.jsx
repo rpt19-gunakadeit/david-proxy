44dbbb7223628ea9e67432d88207860d365ca5ba
davidlira1 <liradavid100@gmail.com>
2020-03-28 12:35:18 -0700

    Moved HTML to React

---
+++ client/App.jsx
@@ -0,0 +1,58 @@
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="ncss-container bg-white">
+                    <section className="d-sm-h d-lg-b">
+                        <div className="l-member-header border-nav-bottom bg-white ncss-row">
+                            <div className="ncss-col-sm-12 prl0-sm">
+                                <ul className="fl-sm-l d-sm-ib">
+                                    <li className="border-nav-right d-sm-ib va-sm-t">
+                                        <a href="https://www.nike.com/membership" className="prl7-sm pt2-sm pb2-sm nav-color-grey fs12-nav-sm d-sm-b" target="_self">
+                                            Join Us
+                                        </a>
+                                    </li>
+                                    <li className="border-nav-right d-sm-ib va-sm-t">
+                                        <a href="https://www.nike.com/jordan" className="prl7-sm pt2-sm pb2-sm nav-color-grey fs12-nav-sm d-sm-b" target="_self">
+                                            <i className="g72-jordan fs20-nav-sm">
+
+                                            </i>
+                                        </a>
+                                    </li>
+                                    <li className="border-nav-right d-sm-ib va-sm-t">
+                                        <a href="https://www.nike.com/hurley" className="prl17-sm pt2-sm pb2-sm nav-color-grey fs12-nav-sm d-sm-b hover-color-black" target="_self">
+                                            <i className="g72-hurley fs14-nav-sm"></i>
+                                        </a>
+                                    
+                                    </li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                <section>
+
+                </section>
+                </nav>
+
+
+
+                <div id="Description"></div>
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
+
