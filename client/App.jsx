0da868f29ed463a6401adfa666bbcee0af35c082
davidlira1 <liradavid100@gmail.com>
2020-04-16 18:54:21 -0700

    added Reb's script for fetching bundle

--- client/App.jsx
+++ client/App.jsx
@@ -136,8 +136,8 @@ class App extends React.Component {
                     <div className="css-rfq3p1 css-1kealuy">
                         <div className="pt4-sm prl6-sm prl0-lg">
                            
-                            <Description 
-                                productId={this.state.productId}/>
+                            {/* <Description 
+                                productId={this.state.productId}/> */}
                         </div>
                         
                         
