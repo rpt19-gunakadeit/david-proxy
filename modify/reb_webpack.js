9f1d9360678b4bd7f487934173c1543c43b3f67a
davidlira1 <liradavid100@gmail.com>
2020-04-17 21:15:08 -0700

    modify npm script for rebekah

--- modify/reb_webpack.js
+++ modify/reb_webpack.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
 
-
-
 var reb_webpackConfig = fs.readFileSync('./../Rebekah-Reviews-service/webpack.config.js', 'utf-8')
-console.log(reb_webpackConfig);
-console.log(reb_webpackConfig.match(/reviews: .*\n/));
-// console.log(reb_webpackConfig.match(/shippingReturns: .*\r\n/))
 
 reb_webpackConfig = reb_webpackConfig.replace(/reviews: .*\n/, "reviews: path.join(__dirname, './client/components/reviews.jsx'),\n");
 reb_webpackConfig = reb_webpackConfig.replace(/shippingReturns: .*\n/, "shippingReturns: path.join(__dirname, './client/components/shippingReturns.jsx')\n");
