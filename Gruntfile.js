5ea438792d7fe7cd14e23853f652754834a7ece9
davidlira1 <liradavid100@gmail.com>
2020-04-18 15:19:42 -0700

    Gruntfile.js - made grunt task for proxy

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,21 @@ module.exports = function(grunt) {
                 region: 'us-west-1',
                 uploadConcurrency: 5
             },
+            upload_proxy : {
+                options: {
+                    bucket: 'fec-nike-bundles',
+                    differential: true
+                },
+                files: [
+                    {expand: true, cwd: 'public/dist', src: 'proxyBundle.js', dest: '/'}
+                ]
+            },
             upload_david : {
                 options: {
                     bucket: 'fec-nike-bundles',
                     differential: true
                 },
                 files: [
-                    {expand: true, cwd: 'public/dist', src: 'proxyBundle.js', dest: '/'},
                     {expand: true, cwd: '../david-service/public/dist', src: 'descriptionBundle.js', dest: '/'},
                     {expand: true, cwd: '../david-service/public/dist', src: 'carouselBundle.js', dest: '/'}
                 ]
@@ -48,6 +56,11 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-aws-s3');
     grunt.loadNpmTasks('grunt-webpack');
+
+    grunt.registerTask('proxy', 'modify,bundle,upload', function() {
+        //BUNDLE and UPLOAD to S3
+        grunt.task.run(['webpack:proxy','aws_s3:upload_proxy'])
+    })
 
     grunt.registerTask('david', 'modify,bundle,upload', function() {
         //BUNDLE and UPLOAD to S3
