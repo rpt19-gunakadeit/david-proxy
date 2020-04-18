6c343c08a9b2bcbab4b7706722d080be34ed9cb7
davidlira1 <liradavid100@gmail.com>
2020-04-17 21:09:52 -0700

    new line seems to be working...

--- modify/reb_webpack.js
+++ modify/reb_webpack.js
@@ -7,6 +7,6 @@ console.log(reb_webpackConfig);
 console.log(reb_webpackConfig.match(/reviews: .*\n/));
 // console.log(reb_webpackConfig.match(/shippingReturns: .*\r\n/))
 
-reb_webpackConfig = reb_webpackConfig.replace(/reviews: .*\r\n/, "reviews: path.join(__dirname, './client/components/reviews.jsx'),\r\n");
-reb_webpackConfig = reb_webpackConfig.replace(/shippingReturns: .*\r\n/, "shippingReturns: path.join(__dirname, './client/components/shippingReturns.jsx')\r\n");
+reb_webpackConfig = reb_webpackConfig.replace(/reviews: .*\n/, "reviews: path.join(__dirname, './client/components/reviews.jsx'),\n");
+reb_webpackConfig = reb_webpackConfig.replace(/shippingReturns: .*\n/, "shippingReturns: path.join(__dirname, './client/components/shippingReturns.jsx')\n");
 fs.writeFileSync('./../Rebekah-Reviews-service/webpack.config.js', reb_webpackConfig);
