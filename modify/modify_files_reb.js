85033cf6f18886ffef52a617203f49205dd5c27c
davidlira1 <liradavid100@gmail.com>
2020-04-18 08:24:28 -0700

    debug: going to see console, and i also modified the replacement string with n at end

--- modify/modify_files_reb.js
+++ modify/modify_files_reb.js
@@ -19,7 +19,7 @@ var modify_files_reb = function() {
     var reb_reviews_jsx = fs.readFileSync(reb_reviews_jsx_path, 'utf-8');
     reb_reviews_jsx = reb_reviews_jsx.replace("const parsedUrl = new URL(window.location.href);", "");
     reb_reviews_jsx = reb_reviews_jsx.replace("const productId = parsedUrl.searchParams.get('');", "console.log(this.props.productId, props.productId)");
-    reb_reviews_jsx = reb_reviews_jsx.replace(/url: .*\n/, "url: 'http://ec2-54-241-130-11.us-west-1.compute.amazonaws.com:2000/reviews/' + this.props.productId + '/date',");
+    reb_reviews_jsx = reb_reviews_jsx.replace(/url: .*\n/, "url: 'http://ec2-54-241-130-11.us-west-1.compute.amazonaws.com:2000/reviews/' + this.props.productId + '/date',\n");
     reb_reviews_jsx = reb_reviews_jsx.replace("<FullReviews showAllReviews={this.showAllReviews.bind(this)} numStars={this.state.avgStars} reviews={this.state.reviews} product={this.props.productDetails}/>", "<FullReviews showAllReviews={this.showAllReviews.bind(this)} numStars={this.state.avgStars} reviews={this.state.reviews} product={this.props.productDetails} productId={this.props.productId}/>");
     fs.writeFileSync(reb_reviews_jsx_path, reb_reviews_jsx);
     //................................................................................................................................
@@ -27,7 +27,7 @@ var modify_files_reb = function() {
     var reb_fullReviews_jsx = fs.readFileSync(reb_fullReviews_jsx_path, 'utf-8');
     reb_fullReviews_jsx = reb_fullReviews_jsx.replace("const parsedUrl = new URL(window.location.href);", "");
     reb_fullReviews_jsx = reb_fullReviews_jsx.replace("const productId = parsedUrl.searchParams.get('');", "");
-    reb_fullReviews_jsx = reb_fullReviews_jsx.replace(/url: .*\n/, "url: 'http://ec2-54-241-130-11.us-west-1.compute.amazonaws.com:2000/reviews/' + this.props.productId + '/' + filter,");
+    reb_fullReviews_jsx = reb_fullReviews_jsx.replace(/url: .*\n/, "url: 'http://ec2-54-241-130-11.us-west-1.compute.amazonaws.com:2000/reviews/' + this.props.productId + '/' + filter,\n");
     fs.writeFileSync(reb_fullReviews_jsx_path, reb_fullReviews_jsx);
     //................................................................................................................................
 }
