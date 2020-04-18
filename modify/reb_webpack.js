291bdfe774ac5d9d717ea0f5dd61cff66a2724fe
davidlira1 <liradavid100@gmail.com>
2020-04-17 21:03:59 -0700

    troubleshooting reb_webpack.js

--- modify/reb_webpack.js
+++ modify/reb_webpack.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 var reb_webpackConfig = fs.readFileSync('./../Rebekah-Reviews-service/webpack.config.js', 'utf-8')
-// console.log(reb_webpackConfig.match(/reviews: .*\r\n/));
+console.log(reb_webpackConfig.match(/reviews: .*\r\n/));
 // console.log(reb_webpackConfig.match(/shippingReturns: .*\r\n/))
 
 reb_webpackConfig = reb_webpackConfig.replace(/reviews: .*\r\n/, "reviews: path.join(__dirname, './client/components/reviews.jsx'),\r\n");
